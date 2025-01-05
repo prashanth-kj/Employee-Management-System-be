0d12136d9a0b2c743c190d5fc8c8810fc0e0dcff
prashanth-kj <kjprasanth98@gmail.com>
2025-01-05 19:51:22 +0530

    updated

--- src/routes/task.js
+++ src/routes/task.js
@@ -2,7 +2,6 @@ import express from 'express'
 import Auth from "../common/auth.js"
 import taskController from '../controller/task.js'
 
-
 const router = express.Router()
 
 router.post('/create',Auth.validate, taskController.createTask)
@@ -13,6 +12,5 @@ router.get('/performance',Auth.validate,taskController.getOverallPerformance)
 router.get('/:id',Auth.validate, taskController.getTaskById)
 
 router.delete('/delete/:id',Auth.validate,taskController.deleteTask)
-
 
 export default router
